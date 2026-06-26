ac35ee63f4a755a2d3f2de1f0e2746c17cb853a2
keshavsoft <nknnkumar@yahoo.com>
2026-06-26 10:52:22 +0530

    KeshavSoft

---
+++ app.js
@@ -0,0 +1,20 @@
+import { exec } from "child_process";
+
+import express from "express";
+
+import { router as routerFromapi } from './api/routes.js';
+
+import setupRoutes from "./routes.js";
+import startServer from "./server.js";
+
+const app = express()
+
+app.use('/api', routerFromapi);
+
+setupRoutes(app);
+
+const { port } = startServer(app);
+
+if (process.env.OPEN_BROWSER === "true") {
+    exec(`start http://localhost:${port}`);
+};
