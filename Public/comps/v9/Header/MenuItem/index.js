56784bd9bc74506603b7fd9a2425ea886d7c8c6a
keshavsoft <nknnkumar@yahoo.com>
2026-06-29 17:12:56 +0530

    focus perfect

--- Public/comps/v9/Header/MenuItem/index.js
+++ Public/comps/v9/Header/MenuItem/index.js
@@ -1,66 +1,31 @@
-import { createAnchor } from "./createAnchor.js";
-import { createIcon } from "./createIcon.js";
+import { getAnchor } from "./getAnchor.js";
+import { getIcon } from "./getIcon.js";
+import { onClick } from "./onClick.js";
 
 class KSMenuItem extends HTMLElement {
     connectedCallback() {
-        const a = createAnchor({
-            inHtmlId: this.getAttribute("ks-id"),
-            inHref: this.getAttribute("ks-href"),
-            inTableName: this.getAttribute("ks-table-name"),
-            inClass: this.getAttribute("ks-class")
-        });
+        const a = getAnchor(this);
+        const svg = getIcon(this);
 
         const text = this.getAttribute("ks-textToShow") || "";
         const className = this.getAttribute("ks-className") || "";
 
-        const svgName = this.getAttribute("ks-svgName") || "";
-        const svgDivClass = this.getAttribute("ks-svgDivClass") || "";
-
-        const svg = createIcon({
-            inSvgName: svgName,
-            inSvgDivClass: svgDivClass
-        });
-
-        const span =
-            document.createElement("span");
-
+        const span = document.createElement("span");
         span.textContent = text;
-
         span.className = className;
 
         while (this.firstChild) {
             a.appendChild(
                 this.firstChild
             );
-        };
+        }
 
         a.append(
             svg,
             span
         );
 
-        a.addEventListener("click", event => {
-            const currentAnchor = event.currentTarget;
-            const menu = currentAnchor
-                .closest("nav")
-                ?.querySelector("#menu");
-
-            console.log("menu : ", menu, currentAnchor);
-
-            menu?.classList.add("hidden");
-
-            const nav = currentAnchor.closest("nav") || currentAnchor.closest("ks-nav");
-            if (nav) {
-                const anchors = nav.querySelectorAll("ks-menu-item a");
-                anchors.forEach(item => {
-                    item.classList.remove("bg-blue-600");
-                    item.classList.add("lg:bg-transparent");
-                });
-            };
-
-            currentAnchor.classList.add("bg-blue-600");
-            currentAnchor.classList.remove("lg:bg-transparent");
-        });
+        a.addEventListener("click", onClick);
 
         this.appendChild(a);
     }
@@ -74,3 +39,5 @@ customElements.define(
 window.KSMenuItem = KSMenuItem;
 
 export default {};
+
+
