05db642c851cc9aeac069a21f1ef95bad6d3258c
keshavsoft <nknnkumar@yahoo.com>
2026-06-29 21:18:30 +0530

    find perfect

--- api/v3/ItemsTable/find/service.js
+++ api/v3/ItemsTable/find/service.js
@@ -1,31 +1,5 @@
 import getData from "./getData.js";
-
-const aggConfig = {
-    sum: {
-        init: 0,
-        update: (accVal, currentVal) => accVal + (Number(currentVal) || 0)
-    },
-    count: {
-        init: 0,
-        update: (accVal, currentVal) => currentVal !== undefined && currentVal !== null ? accVal + 1 : accVal
-    },
-    max: {
-        init: -Infinity,
-        update: (accVal, currentVal) => {
-            const val = Number(currentVal);
-            return !isNaN(val) ? Math.max(accVal, val) : accVal;
-        }
-    },
-    min: {
-        init: Infinity,
-        update: (accVal, currentVal) => {
-            const val = Number(currentVal);
-            return !isNaN(val) ? Math.min(accVal, val) : accVal;
-        }
-    }
-};
-
-const allowedAggFuncs = Object.keys(aggConfig);
+import { aggConfig, allowedAggFuncs } from "./aggConfig.js";
 
 const startFunc = async ({ inColumnName, inTablePath }, inColumnsToSum) => {
     const dataAsArray = await getData({ inTablePath });
