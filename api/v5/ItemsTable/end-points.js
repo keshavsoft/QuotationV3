e22b5fab4dc2d3f35fa73a723033b29d10c8232a
keshavsoft <nknnkumar@yahoo.com>
2026-07-01 14:40:36 +0530

    insert perfect

--- api/v5/ItemsTable/end-points.js
+++ api/v5/ItemsTable/end-points.js
@@ -1,9 +1,13 @@
 import express from 'express';
 
+import funcFrominsertGenPk from './insertGenPk/controller.js';
+
 const tableName = "ItemsTable.json";
 const tablePath = "Data/ItemsTable.json";
 const configPath = "Config/Schemas/ItemsTable.json";
 
 const router = express.Router();
+
+router.post('/insertGenPk', express.json(), (req, res) => funcFrominsertGenPk({ req, res, inTablePath: tablePath, inConfigPath: configPath }));
 
 export { router };
