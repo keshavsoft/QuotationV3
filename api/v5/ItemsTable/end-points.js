44808cc32b61175d420c06f7c4e69c35a99e07d2
keshavsoft <nknnkumar@yahoo.com>
2026-07-01 22:29:25 +0530

    insertWithMeta

--- api/v5/ItemsTable/end-points.js
+++ api/v5/ItemsTable/end-points.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import funcFrominsertGenPk from './insertGenPk/controller.js';
+import funcFrominsertWithMeta from './insertWithMeta/controller.js';
 
 const tableName = "ItemsTable.json";
 const tablePath = "Data/ItemsTable.json";
@@ -9,5 +10,6 @@ const configPath = "Config/Schemas/ItemsTable.json";
 const router = express.Router();
 
 router.post('/insertGenPk', express.json(), (req, res) => funcFrominsertGenPk({ req, res, inTablePath: tablePath, inConfigPath: configPath }));
+router.post('/insertWithMeta', express.json(), (req, res) => funcFrominsertWithMeta({ req, res, inTablePath: tablePath, inConfigPath: configPath }));
 
 export { router };
