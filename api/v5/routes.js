eea24c8d33a12732df6486bc619de4b5508ff2d3
keshavsoft <nknnkumar@yahoo.com>
2026-07-01 15:45:57 +0530

    v5 perfect

--- api/v5/routes.js
+++ api/v5/routes.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { router as routerFromItemsTable } from "./ItemsTable/end-points.js";
+import { router as routerFromBillsTable } from "./BillsTable/end-points.js";
 
 const router = express.Router()
+router.use("/BillsTable", routerFromBillsTable);
 router.use("/ItemsTable", routerFromItemsTable);;
 
 export { router };
