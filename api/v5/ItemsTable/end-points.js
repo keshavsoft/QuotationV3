00f3c9921ded1799cdc8b21e6b63630bd11bde14
keshavsoft <nknnkumar@yahoo.com>
2026-07-03 10:03:54 +0530

    to test

--- api/v5/ItemsTable/end-points.js
+++ api/v5/ItemsTable/end-points.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 import funcFrominsertGenPk from './insertGenPk/controller.js';
 import funcFrominsertWithMeta from './insertWithMeta/controller.js';
+import funcFromgroupBy from './groupBy/controller.js';
+import { checkColumnName, checkColumnsToSum } from './groupBy/middleware.js';
 
 const tableName = "ItemsTable.json";
 const tablePath = "Data/ItemsTable.json";
@@ -11,5 +13,6 @@ const router = express.Router();
 
 router.post('/insertGenPk', express.json(), (req, res) => funcFrominsertGenPk({ req, res, inTablePath: tablePath, inConfigPath: configPath }));
 router.post('/insertWithMeta', express.json(), (req, res) => funcFrominsertWithMeta({ req, res, inTablePath: tablePath, inConfigPath: configPath }));
+router.post('/groupBy/:columnName', express.json(), checkColumnName({ inConfigPath: configPath }), checkColumnsToSum({ inConfigPath: configPath }), (req, res) => funcFromgroupBy({ req, res, inTablePath: tablePath }));
 
 export { router };
