7ec84f426000466cb13b6001db01881299d8b346
keshavsoft <nknnkumar@yahoo.com>
2026-07-09 00:11:42 +0530

    comp 18

---
+++ Public/ks/components/v18/index.js
@@ -0,0 +1,14 @@
+import "./Header/index.js";
+
+import "./dataListInput/index.js";
+
+// import "./inputForVertical/index.js";
+
+import "./inputWithEnter/index.js";
+
+import "./table/index.js";
+import "./vertical/index.js";
+
+if (window?.ksShowLogTree?.components?.main) console.log("KS Components v16 loaded to DOM");
+
+export default {};
