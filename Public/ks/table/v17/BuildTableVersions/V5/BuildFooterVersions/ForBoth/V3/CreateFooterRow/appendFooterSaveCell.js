e94f4dd5a7f7da80d44c8cf588d3087122a11997
keshavsoft <nknnkumar@yahoo.com>
2026-07-09 00:01:09 +0530

    v5

--- Public/ks/table/v17/BuildTableVersions/V5/BuildFooterVersions/ForBoth/V3/CreateFooterRow/appendFooterSaveCell.js
+++ Public/ks/table/v17/BuildTableVersions/V5/BuildFooterVersions/ForBoth/V3/CreateFooterRow/appendFooterSaveCell.js
@@ -1,4 +1,4 @@
-import { startFunc as createSaveButton } from "./SaveButton/v4/start.js";
+import { startFunc as createSaveButton } from "./SaveButton/v5/start.js";
 
 const appendFooterSaveCell = ({ inOnSaveFunc }) => {
     // debugger
