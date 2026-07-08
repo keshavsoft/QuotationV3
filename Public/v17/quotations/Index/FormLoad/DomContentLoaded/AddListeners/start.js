8f9f5d27e6e905f3234a31b0b7b6a22a94047dbf
keshavsoft <nknnkumar@yahoo.com>
2026-07-08 17:53:35 +0530

    v`7

--- Public/v17/quotations/Index/FormLoad/DomContentLoaded/AddListeners/start.js
+++ Public/v17/quotations/Index/FormLoad/DomContentLoaded/AddListeners/start.js
@@ -1,16 +1,10 @@
-import withAmountHtmlId from "./withAmountHtmlId/start.js";
-
 import ShowAllHtmlId from "./ShowAllHtmlId/start.js";
-
-import headHtmlId from "./headHtmlId/start.js";
 
 import headDescHtmlId from "./headDescHtmlId/start.js";
 import createHtmlId from "./createHtmlId/start.js";
 
 const hookAllListeners = () => {
-    withAmountHtmlId();
     ShowAllHtmlId();
-    headHtmlId();
     headDescHtmlId();
     createHtmlId();
 };
