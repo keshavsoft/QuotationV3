426e740d112515092d4360d7ae9fd0f7d0de366e
keshavsoft <nknnkumar@yahoo.com>
2026-07-09 14:02:41 +0530

    stockItems

--- Public/v21/stockItems/Index/FormLoad/DomContentLoaded/AddListeners/start.js
+++ Public/v21/stockItems/Index/FormLoad/DomContentLoaded/AddListeners/start.js
@@ -1,7 +1,9 @@
-import ShowAllHtmlId from "./ShowAllHtmlId/start.js";
+import headDescHtmlId from "./headDescHtmlId/start.js";
+import createHtmlId from "./createHtmlId/start.js";
 
 const hookAllListeners = () => {
-    ShowAllHtmlId();
+    headDescHtmlId();
+    createHtmlId();
 };
 
 export { hookAllListeners };
