fffe6fb4961240c1a5746389fdff247b400490ae
keshavsoft <nknnkumar@yahoo.com>
2026-07-10 18:10:05 +0530

    data shown

--- Public/v23/purExpVouchers/Index/FormLoad/DomContentLoaded/AddListeners/headDescHtmlId/BuildTable/helpers/getData.js
+++ Public/v23/purExpVouchers/Index/FormLoad/DomContentLoaded/AddListeners/headDescHtmlId/BuildTable/helpers/getData.js
@@ -1,6 +1,23 @@
+const expandLedgerData = (inData) => {
+    const toReturnArray = [];
+
+    inData.forEach(element => {
+        element.allledgerentries.forEach(loopLedgerentries => {
+            toReturnArray.push({
+                ...element,
+                ledgername: loopLedgerentries.ledgername,
+                amount: loopLedgerentries.amount
+            });
+        });
+    });
+
+    return toReturnArray;
+};
+
 export const getData = async () => {
     const config = await fetch("/api/v7/purExpVouchers/showAll");
     const data = await config.json();
 
-    return await data;
+    return await expandLedgerData(data);
 };
+
