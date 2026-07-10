2f8c9226a81687effed059eb6adeeac3a3404e6d
keshavsoft <nknnkumar@yahoo.com>
2026-07-10 17:59:26 +0530

    pur exp

--- api/v7/routes.js
+++ api/v7/routes.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { router as routerFromLedgerNames } from "./LedgerNames/end-points.js";
+import { router as routerFrompurExpVouchers } from "./purExpVouchers/end-points.js";
 
 const router = express.Router()
+router.use("/purExpVouchers", routerFrompurExpVouchers);
 router.use("/LedgerNames", routerFromLedgerNames);;
 
 export { router };
