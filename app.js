f389deb237feb4d9bbc000c319f21ada3000d33a
keshavsoft <nknnkumar@yahoo.com>
2026-07-11 15:54:49 +0530

    v26

--- app.js
+++ app.js
@@ -18,5 +18,5 @@ setupRoutes(app);
 const { port } = startServer(app);
 
 if (process.env.OPEN_BROWSER === "true") {
-    exec(`start http://localhost:${port}/v25/quotations/index.html`);
+    exec(`start http://localhost:${port}/v26/quotations/index.html`);
 };
