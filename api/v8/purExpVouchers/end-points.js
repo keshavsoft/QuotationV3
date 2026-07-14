e60c398ca9b5015a7e1027595f4e1af831a03fbb
keshavsoft <nknnkumar@yahoo.com>
2026-07-14 17:48:55 +0530

    array

--- api/v8/purExpVouchers/end-points.js
+++ api/v8/purExpVouchers/end-points.js
@@ -4,6 +4,8 @@ import funcFromshowAll from './showAll/controller.js';
 import funcFromdistinct from './distinctDate/controller.js';
 import funcFromcount from './count/controller.js';
 import funcFromDistinctLedgerName from './distinctLedgerName/controller.js';
+import funcFromgroupBy from './groupBy/controller.js';
+import { checkColumnName, checkColumnsToSum } from './groupBy/middleware.js';
 
 const tableName = "purExpVouchers.json";
 const tablePath = "Data/purExpVouchers.json";
@@ -15,5 +17,6 @@ router.get('/showAll', (req, res) => funcFromshowAll({ req, res, inTablePath: ta
 router.get('/distinctDate', (req, res) => funcFromdistinct({ req, res, inTablePath: tablePath }));
 router.get('/count', (req, res) => funcFromcount({ req, res, inTablePath: tablePath }));
 router.get('/distinctLedgerName', (req, res) => funcFromDistinctLedgerName({ req, res, inTablePath: tablePath }));
+router.post('/groupBy/:columnName', express.json(), (req, res) => funcFromgroupBy({ req, res, inTablePath: tablePath }));
 
 export { router };
